41d1212db6d8d28fe0f977d078e8bba138664522
Abhinandan Jain <nandanjain04@gmail.com>
2025-03-16 21:16:43 +0530

    Moving files

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
+#include "main.h"
 
 int main()
 {
-	printf("Full Form Oriented Compiler");
-	return 0;
+	if (preprocessor("file.ffo", "raw_code.ffo") == INCOMPLETE)
+	{
+		return INCOMPLETE;
+	}
+
+	if (init_lexer("raw_code.ffo") == INCOMPLETE)
+	{
+		return INCOMPLETE;
+	}
+
+	TOKEN *token = get_next_token();
+	do
+	{
+		printf("TOKEN TYPE: %d\tLEXEME: %s\n", token->type, token->value);
+		free(token);
+		token = get_next_token();
+	} while (token->type != TOKEN_EOF);
+
+	free_lexer();
+
+	return COMPLETE;
 }
